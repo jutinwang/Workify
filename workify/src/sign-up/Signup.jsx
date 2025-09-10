eb23a829f6dbe1a69817ca0c239400d4d8b4e6a3
Ali B <alibhangu1@outlook.com>
2025-09-10 14:51:06 -0400

    Finished left side container for Sign up

--- workify/src/sign-up/Signup.jsx
+++ workify/src/sign-up/Signup.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./Signup.css";
+import metaLogo from "../assets/metaLogo.png";
+import scoLogo from "../assets/scoLogo.png";
+import zonLogo from "../assets/zonLogo.png";
+import sunLifeLogo from "../assets/sunLifeLogo.png";
 
 const Signup = () => {
   return (
     <div className="split-container">
       <div className="left-side-container">
-
-        
         <div className="text-container">
           <h2 className="logo-workify">WORKIFY</h2>
 
@@ -15,11 +17,27 @@ const Signup = () => {
             <span>Start Applying</span>
           </div>
 
-        <div className="subtext">
-        <span>Pick from the best job postings here on <span style={{fontWeight: "bold"}}>Workify</span></span>
-        </div>
+          <div className="subtext">
+            <span>
+              Pick from the best job postings here on{" "}
+              <span style={{ fontWeight: "bold", fontFamily: "Rosarivo"}}>Workify</span>
+            </span>
+          </div>
 
-
+          <div class="brand-row">
+            <div class="brand-chip">
+              <img src={metaLogo} alt="Meta" />
+            </div>
+            <div class="brand-chip">
+              <img src={scoLogo} alt="Scotiabank" />
+            </div>
+            <div class="brand-chip">
+              <img src={zonLogo} alt="Amazon" />
+            </div>
+            <div class="brand-chip">
+              <img src={sunLifeLogo} alt="Sun Life" />
+            </div>
+          </div>
         </div>
       </div>
 
