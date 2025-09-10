1962c6fca9c931a1c4d9c512d4180e2e690ab2c6
Ali B <alibhangu1@outlook.com>
2025-09-10 14:40:37 -0400

    fixed left-side container

--- workify/src/sign-up/Signup.jsx
+++ workify/src/sign-up/Signup.jsx
@@ -5,11 +5,26 @@ const Signup = () => {
   return (
     <div className="split-container">
       <div className="left-side-container">
+
         
+        <div className="text-container">
+          <h2 className="logo-workify">WORKIFY</h2>
+
+          <div className="text-blocks">
+            <span>Create an Account </span>
+            <span>Start Applying</span>
+          </div>
+
+        <div className="subtext">
+        <span>Pick from the best job postings here on <span style={{fontWeight: "bold"}}>Workify</span></span>
+        </div>
+
+
+        </div>
       </div>
+
       <div className="right-side-container">
         <h2>Hey</h2>
-
       </div>
     </div>
   );
