7c3256a2ee8f2f003fa1bf917f1768144358d9c2
Ali B <alibhangu1@outlook.com>
2025-09-11 13:41:06 -0400

    initial commit for profile view

---
+++ workify/src/profile/profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../common/Header';
+import "./Profile.css";
+
+const Profile = () => {
+    return (
+        <div className='main'>
+            <Header />
+            <h1>Profile</h1>
+        </div>
+    );
+};
+
+export default Profile;
