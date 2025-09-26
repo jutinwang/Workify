073a89d6b2b89cbf93a2ec7e827d8b1f035487af
Ali B <alibhangu1@outlook.com>
2025-09-26 10:48:35 -0400

    adding the bookmark icon to job listings

--- workify/src/jobs/JobCard.jsx
+++ workify/src/jobs/JobCard.jsx
@@ -29,6 +29,11 @@ export default function JobCard({ job, isSelected, onClick }) {
                 {job.remote && <span className="pill">Remote</span>}
                 </div>
             </div>
+            <button className="bookmark-btn" onClick={(e) => e.stopPropagation()}>
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
+            </button>
             </div>
     
             {/* Summary */}
