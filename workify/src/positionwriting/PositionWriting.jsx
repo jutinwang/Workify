ceb721f53bb828abc8a6aef6484847a34d87e809
justinwang <justinwang407@gmail.com>
2025-10-22 17:01:14 -0400

    added tags and changed style

--- workify/src/positionwriting/PositionWriting.jsx
+++ workify/src/positionwriting/PositionWriting.jsx
@@ -2,9 +2,6 @@ import { useState, useRef } from "react";
 import './PositionWriting.css';
 
 const PositionWriting = () => {
-    // const [formData] = useState({
-    //     jobtags: ["Software", "Agile", "Front-end", "Teamwork"]
-    // });
     const [coopDescription, setCoopDescription] = useState("");
     const [responsibilities, setResponsibilities] = useState("");
     const [qualifications, setQualifications] = useState("");
@@ -12,6 +9,7 @@ const PositionWriting = () => {
     const [jobLength, setJobLength] = useState("");
     const [salaryRange, setSalaryRange] = useState("");
     const [workModel, setWorkModel] = useState("");
+    const[tags, setTags] = useState([])
 
     const textareaRef = useRef(null);
 
@@ -31,6 +29,17 @@ const PositionWriting = () => {
         setSalaryRange(salary.target.value);
     };
 
+    function handleKeyDown(e) {
+        if (e.key !== 'Enter') return
+        const value = e.target.value
+        if (!value.trim()) return
+        setTags([...tags, value])
+        e.target.value = ''
+    }
+
+    function removeTag(index) {
+        setTags(tags.filter((el, i) => i !== index))
+    }
 
     return (
         <div className="positionwriting-container">
@@ -38,143 +47,53 @@ const PositionWriting = () => {
                 <h3>Create Coop Posting</h3>
                 <div className="textInput">
                     <p>Coop Description</p>
-                    <textarea className="textInput"
+                    <textarea className="infoInput"
                         ref={textareaRef}
                         value={coopDescription}
                         onChange={handleCoopDescriptionChange}
                         placeholder="Type something..."
-                        style={{
-                            minHeight: '40px', // Optional: set a minimum height
-                            resize: 'none',    // Prevent manual resizing by user
-                            overflowY: 'auto',
-                            overflow: 'hidden', // Hide scrollbar
-                            width: '100%',     // Adjust as needed
-                            height: '250px',
-                            border: '1.5px',
-                            borderStyle: 'solid',
-                            borderColor: 'black',
-                            borderRadius: '8px',
-                            padding: '10px',
-                            fontSize: '14px',
-                            boxSizing: 'border-box'
-                        }}
                     />
 
                      <p>Key Resposibilities</p>
-                    <textarea className="textInput"
+                    <textarea className="infoInput"
                         ref={textareaRef}
                         value={responsibilities}
                         onChange={handleResponsibilitiesChange}
                         placeholder="Type something..."
-                        style={{
-                            minHeight: '40px', // Optional: set a minimum height
-                            resize: 'none',    // Prevent manual resizing by user
-                            overflowY: 'auto',
-                            overflow: 'hidden', // Hide scrollbar
-                            width: '100%',     // Adjust as needed
-                            height: '250px',
-                            border: '1.5px',
-                            borderStyle: 'solid',
-                            borderColor: 'black',
-                            borderRadius: '8px',
-                            padding: '10px',
-                            fontSize: '14px',
-                            boxSizing: 'border-box'
-                        }}
                     />
 
                     <p>Qualifications</p>
-                    <textarea className="textInput"
+                    <textarea className="infoInput"
                         ref={textareaRef}
                         value={qualifications}
                         onChange={handleQualificationsChange}
                         placeholder="Type something..."
-                        style={{
-                            minHeight: '40px', // Optional: set a minimum height
-                            resize: 'none',    // Prevent manual resizing by user
-                            overflowY: 'auto',
-                            overflow: 'hidden', // Hide scrollbar
-                            width: '100%',     // Adjust as needed
-                            height: '250px',
-                            border: '1.5px',
-                            borderStyle: 'solid',
-                            borderColor: 'black',
-                            borderRadius: '8px',
-                            padding: '10px',
-                            fontSize: '14px',
-                            boxSizing: 'border-box'
-                        }}
                     />
 
                     <p>Benefits and Perks</p>
-                    <textarea className="textInput"
+                    <textarea className="infoInput"
                         ref={textareaRef}
                         value={benefits}
                         onChange={handleBenefetsChange}
                         placeholder="Type something..."
-                        style={{
-                            minHeight: '40px', // Optional: set a minimum height
-                            resize: 'none',    // Prevent manual resizing by user
-                            overflowY: 'auto',
-                            overflow: 'hidden', // Hide scrollbar
-                            width: '100%',     // Adjust as needed
-                            height: '250px',
-                            border: '1.5px',
-                            borderStyle: 'solid',
-                            borderColor: 'black',
-                            borderRadius: '8px',
-                            padding: '10px',
-                            fontSize: '14px',
-                            boxSizing: 'border-box'
-                        }}
                     />
 
                     <p>Salary Range</p>
-                    <textarea className="textInput"
+                    <textarea className="infoInput"
                         ref={textareaRef}
                         value={salaryRange}
                         onChange={handleSalaryChange}
                         placeholder="Type something..."
-                        style={{
-                            minHeight: '40px', // Optional: set a minimum height
-                            resize: 'none',    // Prevent manual resizing by user
-                            overflowY: 'auto',
-                            overflow: 'hidden', // Hide scrollbar
-                            width: '100%',     // Adjust as needed
-                            height: '45px',
-                            border: '1.5px',
-                            borderStyle: 'solid',
-                            borderColor: 'black',
-                            borderRadius: '8px',
-                            padding: '10px',
-                            fontSize: '14px',
-                            boxSizing: 'border-box'
-                        }}
                     />
                     
                     <div className="dropdowns">
                         <p>Job Length</p>
                         <select
-                            className="dropdown"
+                            className="dropdown job-length-dropdown"
                             value={jobLength}
                             onChange={(e) => setJobLength(e.target.value)}
-                            style={{
-                                minHeight: '40px', // Optional: set a minimum height
-                                resize: 'none',    // Prevent manual resizing by user
-                                overflowY: 'auto',
-                                overflow: 'hidden', // Hide scrollbar
-                                width: '45%',     // Adjust as needed
-                                height: '45px',
-                                border: '1.5px',
-                                borderStyle: 'solid',
-                                borderColor: 'black',
-                                borderRadius: '8px',
-                                padding: '10px',
-                                fontSize: '14px',
-                                boxSizing: 'border-box'
-                            }}
                         >
-                            <option value="">Select duration...</option>
+                            <option value="" disabled selected>Select duration...</option>
                             <option value="4">4 months</option>
                             <option value="8">8 months</option>
                             <option value="12">12 months</option>
@@ -183,39 +102,28 @@ const PositionWriting = () => {
 
                         <p>Work Models</p>
                         <select
-                            className="dropdown"
+                            className="dropdown work-model-dropdown"
                             value={workModel}
                             onChange={(e) => setWorkModel(e.target.value)}
-                            style={{
-                                minHeight: '40px', // Optional: set a minimum height
-                                resize: 'none',    // Prevent manual resizing by user
-                                overflowY: 'auto',
-                                overflow: 'hidden', // Hide scrollbar
-                                width: '45%',     // Adjust as needed
-                                height: '45px',
-                                border: '1.5px',
-                                borderStyle: 'solid',
-                                borderColor: 'black',
-                                borderRadius: '8px',
-                                padding: '10px',
-                                fontSize: '14px',
-                                boxSizing: 'border-box'
-                            }}
                         >
-                            <option value="">Select work model...</option>
+                            <option value="" disabled selected>Select work model...</option>
                             <option value="remote">Remote</option>
                             <option value="hybrid">Hybrid</option>
                             <option value="inperson">In-Person</option>
                         </select>
                     </div>
                 </div>
-                {/* <div className="tags-container">
-                    {formData.jobtags.map((tag, idx) => (
-                        <div key={idx} className="tag">
-                        <span>X</span> {tag}
+
+                <div className="tags-container">
+                    { tags.map((tag, index) => (
+                        <div className="tag-item" key={index}>
+                            <span className="text">{tag}</span>
+                            <span className="close" onClick={() => removeTag(index)}>&times;</span>
                         </div>
                     ))}
-                </div> */}
+                    <input onKeyDown={handleKeyDown} type="text" className="tags-input" placeholder="Type Something"></input>
+                </div>
+
                 <div className="button-container">
                     <button className="cancel-btn">Cancel</button>
                     <button className="post-btn">Post</button>
